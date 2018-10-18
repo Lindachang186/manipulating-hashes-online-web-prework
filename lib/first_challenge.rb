b6e21489580904a6445d24579400caa60ce60a3a
Linda Chang <lindachang186@gmail.com>
2018-10-18 15:40:14 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   def first_challenge(flavor)
         contacts["Freddy Mercury"].each do |key, value|
           if key == :favorite_icecream_flavors
-              :favorite_icecream_flavors.delete_if {|value| value == "strawberry"}
+              :favorite_icecream_flavors.delete_if {|flavor| flavor == "strawberry"}
       end
       return contacts
   end
